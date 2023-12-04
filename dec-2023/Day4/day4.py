6df554912ed5ff96a1ab1a9b0123366d648ee092
Monika <monidavies@gmail.com>
2023-12-04 12:29:07 -0800

    Adds Day4 part 2

--- dec-2023/Day4/day4.py
+++ dec-2023/Day4/day4.py
@@ -11,23 +11,55 @@ test_card = [
   "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
 ]
 
-def getPoints(cards):
+# Part 2 main function
+def countCards(cards):
+  res = 0
+  counts = getMatchCounts(cards) # [[card_id_number, matches, # of copies]]
+
+  for card_id, points, num in counts:
+    i = card_id - 1
+    for p in range(1, points + 1):
+      counts[i+p][2] += counts[i][2]
+  for card in counts:
+    res += card[2]
+  return res
+
+def getMatchCounts(cards):
+  # Initializes point_counts array
+  match_counts = [[0, 0, 1] for _ in range(len(cards))] # [card_id_number, matches, # of copies]
+  for i, card in enumerate(cards):
+    winNums, playNums = getNums(card)
+    matches = getMatches(winNums, playNums)
+    match_counts[i][0] = i + 1
+    match_counts[i][1] = matches
+  
+  return match_counts
+
+def getMatches(winNums, playNums):
+  winners = set(winNums)
+  return sum(num in playNums for num in winNums)
+
+
+# Part 1 main function
+def getTotalPoints(cards):
   res = 0
   for card in cards:
     winNums, playNums = getNums(card)
-    
-    winSet = set()
-    count = 0
-    for num in winNums:
-      winSet.add(num)
-    for num in playNums:
-      if num in winSet:
-        count += 1
-    points = 2 ** (count - 1) if count > 0 else 0
+    points = getPoints(winNums, playNums)
     res += points
   return res
 
+def getPoints(winNums, playNums):
+  winSet = set(winNums)
+  count = 0
+  for num in playNums:
+    if num in winSet:
+      count += 1
+  points = 2 ** (count - 1) if count > 0 else 0
+  return points
+
 def getNums(card):
+  # Process string to get both sets of numbers
   card = card.split(':')[1]
   card.strip()
   nums1, nums2 = card.split('|')
@@ -43,5 +75,8 @@ def getNums(card):
 
 
 if __name__ == "__main__":
-  total = getPoints(lines)
+  # total = getPoints(lines)
+  # print(total)
+
+  total = countCards(lines)
   print(total)
