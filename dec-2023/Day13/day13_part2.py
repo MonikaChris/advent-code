b07e540a5182566c90673f64b69265fcc7ae882d
Monika <monidavies@gmail.com>
2023-12-19 20:10:57 -0800

    Adds Day13 part 2

--- dec-2023/Day13/day13_part2.py
+++ dec-2023/Day13/day13_part2.py
@@ -1,12 +1,13 @@
-test = [
-  ['#.##..##.',
+test = [[
+  '#.##..##.',
   '..#.##.#.',
   '##......#',
   '##......#',
   '..#.##.#.',
   '..##..##.',
   '#.#.##.#.'],
-  ['#...##..#',
+  [
+  '#...##..#',
   '#....#..#',
   '..##..###',
   '#####.##.',
@@ -14,26 +15,6 @@ test = [
   '..##..###',
   '#....#..#']
 ]
-
-test2 = [[
-  '######.##',
-  '##..###..',
-  '.......#.',
-  '#.##.#..#',
-  '###.####.',
-  '######..#',
-  '.#..#.#.#',
-  '#....###.',
-  '.......##',
-  '.####.##.',
-  '#....#...',
-  '..##....#',
-  '..##....#',
-  '######...',
-  '.#..#..##',
-  '.#..#..##',
-  '######...'
-]]
 
 with open('Day13/data.txt', 'r') as file:
   lines = file.readlines()
@@ -50,10 +31,76 @@ if pattern:
   patterns.append(pattern)
 
 
+def main(patterns):
+  res = 0
+  for pattern in patterns:
+    res += get_reflection(pattern)
+  return res
 
+def get_reflection(pattern):
+  cols = get_mirror(rotate(pattern))
+  if cols:
+    return cols
+  else:
+    rows = get_mirror(pattern) * 100
+    return rows
+
+
+def get_mirror(pattern):
+  # Make grid of row index by row index where element is number of characters that differ
+  ROWS = len(pattern)
+  grid = [[0 for _ in range(ROWS)] for _ in range(ROWS)]
   
+  for r in range(ROWS):
+    for c in range(ROWS):
+      grid[r][c] = get_diff(pattern, r, c)
+  return find_line(grid)
+  
+
+def get_diff(pattern, i, j):
+  row1 = pattern[i]
+  row2 = pattern[j]
+
+  count = 0
+  for i in range(len(row1)):
+    if row1[i] != row2[i]:
+      count += 1
+  return count
+  
+
+def find_line(grid):
+  # Check odd diagonals, look for sum of 2 (1 double counted)
+  ROWS = len(grid)
+
+  diagonals = [[] for _ in range(ROWS * 2 - 1)]
+  for r in range(ROWS):
+    for c in range(ROWS):
+      if (r + c) % 2 == 1:
+        diagonals[r + c].append(grid[r][c])
+  
+  for i, arr in enumerate(diagonals):
+    if sum(arr) == 2:
+      return i//2 + 1
+  return None
+
+
+def rotate(grid):
+  ROWS = len(grid)
+  COLS = len(grid[0])
+
+  new_grid = []
+
+  for c in range(COLS):
+    new_row = []
+    for r in range(ROWS):
+      new_row.append(grid[r][c])
+    new_grid.append(new_row)
+
+  return new_grid
+
 
 
 if __name__ == "__main__":
   print(main(patterns))
+  
   
