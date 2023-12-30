e408baac81a36456da8b69d4b700c0844a5bfff5
Monika <monidavies@gmail.com>
2023-12-29 21:40:49 -0800

    Adds Day17 part1 partial

---
+++ dec-2023/Day17/day17.py
@@ -0,0 +1,49 @@
+with open('Day17/data.txt', 'r') as file:
+  lines = file.readlines()
+lines = [[int(char) for char in list(line.strip())] for line in lines]
+
+
+with open('Day17/test.txt', 'r') as file:
+  test = file.readlines()
+test = [[int(char) for char in list(line.strip())] for line in test]
+
+dirs = {
+  "N" : (-1, 0),
+  "S" : (1, 0),
+  "E" : (0, 1),
+  "W" : (0, -1)
+}
+
+
+def main(grid):
+  ROWS = len(grid)
+  COLS = len(grid[0])
+  res = [float("inf")]
+
+  def dfs(r, c, s, d, weight, visited): # row, col, step_count, direction, weight, visited
+    if r < 0 or r >= ROWS or c < 0 or c >= COLS or (r, c) in visited:
+      return
+    
+    if r == ROWS - 1 and c == COLS - 1:
+      res[0] = min(res[0], weight)
+      return
+
+    visited.add((r, c))
+
+    for dir in dirs.keys():
+      if d != dir:
+        dfs(r + dirs[dir][0], c + dirs[dir][1], 1, dir, weight + grid[r][c], visited)
+
+      if d == dir and s < 3:
+        dfs(r + dirs[dir][0], c + dirs[dir][1], s + 1, dir, weight + grid[r][c], visited)
+  
+    return
+
+  dfs(0, 1, 1, "E", 0, set())
+  dfs(1, 0, 1, "S", 0, set())
+  return res[0]
+
+
+if __name__ == "__main__":
+  print(main(test))
+  
