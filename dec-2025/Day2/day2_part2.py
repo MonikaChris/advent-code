cef659509087ba434f0b4d6b7dfff347cc75c3bc
Monika <monidavies@gmail.com>
2025-12-14 16:11:48 -0800

    Adds Day2

---
+++ dec-2025/Day2/day2_part2.py
@@ -0,0 +1,43 @@
+with open("Day2/data.txt", "r") as file:
+  data = file.read().strip().split(",")
+
+sampleData = [
+  "11-22",
+  "95-115",
+  "998-1012",
+  "1188511880-1188511890",
+  "222220-222224",
+  "1698522-1698528",
+  "446443-446449",
+  "38593856-38593862",
+  "565653-565659",
+  "824824821-824824827",
+  "2121212118-2121212124"
+]
+
+def isRepeatedSequence(num):
+  factors = [i for i in range(1, len(num)//2 + 1) if len(num) % i == 0]
+  
+  for factor in factors:
+    part = num[:factor]
+    i = factor
+    while i + factor < len(num) + 1:
+      if num[i: i + factor] != part:
+        break
+      if i + factor >= len(num):
+        return True
+      i += factor
+  return False
+      
+def findInvalidIds(idRanges):
+  count = 0
+  for idRange in idRanges:
+    start, end = idRange.split("-")
+    for num in range(int(start), int(end) + 1):
+      if isRepeatedSequence(str(num)):
+        count += num
+  return count
+
+if __name__ == "__main__":
+  print(findInvalidIds(sampleData))
+  print(findInvalidIds(data))
